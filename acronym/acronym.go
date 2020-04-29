38b0113eee9925aef7bf05aa986bf82823ce6af9
svemaraju <srikanth.vemaraju@gmail.com>
2020-04-29 19:00:13 +0530

    Submit acronym.

---
+++ acronym/acronym.go
@@ -0,0 +1,32 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package acronym should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package acronym
+
+import (
+	"strings"
+	"unicode"
+)
+
+// Abbreviate should have a comment documenting it.
+func Abbreviate(s string) string {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+	var x string
+	var prevLetter rune
+	var apostrophe = "'"
+	for _, w := range s {
+		if string(w) == apostrophe {
+			continue
+		}
+		if unicode.IsLetter(w) && !unicode.IsLetter(prevLetter) {
+			x += string(w)
+		}
+		prevLetter = w
+	}
+	return strings.ToUpper(x)
+}
