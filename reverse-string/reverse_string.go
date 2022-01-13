e2ceacabd5b0d2f6415c3803b9afb4d6974b07f6
Srikanth Vemaraju <srikanth.vemaraju@hedgehoglab.com>
2022-01-13 12:49:45 +0000

    Complete reversing a string in go.

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,16 +1,20 @@
 package reverse
 
-func getReversedString(wordA rune, wordB []rune) runes {
-	// fmt.Println(wordA, wordB)
+// getReversedString is recursive function
+// to reverse a string
+func getReversedString(wordA rune, wordB []rune) string {
+    if len(wordB) == 0 {
+        return string(wordA)
+    }
 	if len(wordB) == 1 {
-		return wordB + wordA
+		return string(wordB[0]) + string(wordA)
 	}
-	return getReversedString(string(wordB[0]), wordB[1:]) + wordA
+	return getReversedString(wordB[0], wordB[1:]) + string(wordA)
 }
 
 // Reverse returns the reverse of string.
 func Reverse(word string) string {
-	wordRunes := []rune(s)
+	wordRunes := []rune(word)
 	if len(wordRunes) == 0 || len(word) == 1 {
 		return string(wordRunes)
 	}
